720f5a26717a461b1e364ab311c76b8b73738b5d
Florian Lanz <florianlanz73@gmail.com>
2021-04-28 23:39:06 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Rocket-Godot
+ This is a simple rocket game. Goal of the game is to land a rocket on a solid block
---
+++ scenes/rocket.tres.gd
@@ -0,0 +1,17 @@
+extends Sprite
+
+
+# Declare member variables here. Examples:
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_pressed("up"):
+		position.y -= 100*delta
+	pass
