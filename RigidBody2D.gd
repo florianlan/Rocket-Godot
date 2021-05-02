71f324b0051afaa500cb9a972c7c7a092a6fd12c
Florian Lanz <florianlanz73@gmail.com>
2021-05-02 20:00:52 +0200

    Changing working from laptop to pc

--- RigidBody2D.gd
+++ RigidBody2D.gd
@@ -1,8 +1,7 @@
-extends Sprite
+extends RigidBody2D
 
 
 # Declare member variables here. Examples:
-
 
 
 # Called when the node enters the scene tree for the first time.
@@ -12,5 +11,14 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	var thrust = Vector2(0, 0)
+	
+	if Input.is_action_pressed("up"):
+		thrust.y = -5
+		#thrust.x = rotation*5
+		apply_impulse(thrust, thrust)
+		
+	if Input.is_action_pressed("right"):
+		
 	
 	pass
